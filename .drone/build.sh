138bf19ba948f2625fc121e0abc0d9b0d7c61278
calvinpy <dani@aplazame.com>
2015-08-25 05:19:44 +0200

    deploy on master

--- .drone/build.sh
+++ .drone/build.sh
@@ -5,15 +5,11 @@ set -e
 make init-test
 
 case $DRONE_BRANCH in
-    dev)
-        echo pull request
-        ;;
-
     master|release)
         make coverage
         ;;
 
     *)
-        echo "Unknown"
+        echo pull request, all checks have passed
         ;;
 esac
