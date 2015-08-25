762639dce9fc108078cf788209c09988a62c2252
calvinpy <dani@aplazame.com>
2015-08-25 13:56:14 +0200

    fix amount is below the allowable limit

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -6,5 +6,5 @@ from .base import PublicTestCase
 class SimulatorTestCase(PublicTestCase):
 
     def test_get_simulator(self):
-        response = self.client.simulator(amount=random.randint(100, 1000))
+        response = self.client.simulator(amount=random.randint(1000, 10000))
         self.assertEqual(response.status_code, 200)
